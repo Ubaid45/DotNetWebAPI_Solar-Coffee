387bb03aacfd99606370c1f575c4c0ea24e49491
Ubaid Rana <ubaidrana45@gmail.com>
2020-05-21 00:50:47 +0200

    Added methods into IOrder interface

--- SolarCoffee.Services/Order/IOrderService.cs
+++ SolarCoffee.Services/Order/IOrderService.cs
@@ -1,7 +1,10 @@
-namespace SolarCoffee.Services.Order
-{
-    public interface IOrderService
-    {
-        
+using System.Collections.Generic;
+using SolarCoffee.Data.Models;
+
+namespace SolarCoffee.Services.Order {
+    public interface IOrderService {
+        List<SalesOrder> GetOrders();
+        ServiceResponse<bool> GenerateOpenOrder(SalesOrder order);
+        ServiceResponse<bool> MarkFulfilled(int id);
     }
 }

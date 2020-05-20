446a0418d3494adfb2333025da78c82fcb241811
Ubaid Rana <ubaidrana45@gmail.com>
2020-05-21 00:41:25 +0200

    Created Order controller

---
+++ SolarCoffee.Web/Controllers/OrderController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SolarCoffee.Web.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return null;
+        }
+    }
+}

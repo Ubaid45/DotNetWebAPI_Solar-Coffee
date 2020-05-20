f37037f67ce0a544dbbacd479311b3e3bd136f2c
Ubaid Rana <ubaidrana45@gmail.com>
2020-05-20 22:19:07 +0200

    Added Models and updated DBContext

--- SolarCoffee.Data/SolarDbContext.cs
+++ SolarCoffee.Data/SolarDbContext.cs
@@ -1,16 +1,25 @@
+using System.Configuration;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using SolarCoffee.Data.Models;
 
-namespace SolarCoffee.Data
-{
-    public class SolarDbContext : IdentityDbContext
-    {
-        public SolarDbContext() {}
+namespace SolarCoffee.Data {
+    public class SolarDbContext : IdentityDbContext {
+        public SolarDbContext() { }
 
         public SolarDbContext(DbContextOptions options) : base(options) { }
-        
-        public virtual DbSet<Customers> Customers { get; set; }
-        public virtual DbSet<CustomerAddress> CustomerAddress { get; set; }
+        protected override void OnConfiguring( DbContextOptionsBuilder optionsBuilder )
+        {
+            // TODO: This is messy, but needed for migrations.
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Username=solardev;Password=solar123;Database=solardev;");
+            
+        }
+        public virtual DbSet<Customer> Customers { get; set; }
+        public virtual DbSet<CustomerAddress> CustomerAddresses { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<ProductInventory> ProductInventories { get; set; }
+        public virtual DbSet<ProductInventorySnapshot> ProductInventorySnapshots { get; set; }
+        public virtual DbSet<SalesOrder> SalesOrders { get; set; }
+        public virtual DbSet<SalesOrderItem> SalesOrderItems { get; set; }
     }
 }

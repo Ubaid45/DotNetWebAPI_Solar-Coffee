2dd80177e66bcde1e2519039077c1716fdf1f146
Ubaid Rana <ubaidrana45@gmail.com>
2020-05-21 00:40:10 +0200

    Implemented end-points of Product controller

--- SolarCoffee.Web/Controllers/ProductController.cs
+++ SolarCoffee.Web/Controllers/ProductController.cs
@@ -1,35 +1,62 @@
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using SolarCoffee.Data.Models;
 using SolarCoffee.Services.Product;
-using System.Net.Http;
 using SolarCoffee.Web.Serialization;
+using SolarCoffee.Web.ViewModels;
+using System.Net.Http;
 
-namespace SolarCoffee.Web.Controllers
-{
+namespace SolarCoffee.Web.Controllers {
+    
     [ApiController]
-    public class ProductController : ControllerBase
-    {
+    public class ProductController : ControllerBase {
         private readonly ILogger<ProductController> _logger;
         private readonly IProductService _productService;
 
-        public ProductController(ILogger<ProductController> logger, IProductService productService)
-        {
+        public ProductController(ILogger<ProductController> logger, IProductService productService) {
             _logger = logger;
             _productService = productService;
         }
-        
+
+        /// <summary>
+        /// Adds a new product
+        /// </summary>
+        /// <param name="product"></param>
+        /// <returns></returns>
+        [HttpPost("/api/products")]
+        public ActionResult AddProduct([FromBody] ProductModel product) {
+            if (!ModelState.IsValid) {
+                return BadRequest(ModelState);
+            }
+            _logger.LogInformation("Adding product");
+            var newProduct = ProductMapper.SerializeProductModel(product);
+            var newProductResponse = _productService.CreateProduct(newProduct);
+            return Ok(newProductResponse);
+        }
+
         /// <summary>
         /// Returns all products
         /// </summary>
         /// <returns></returns>
         [HttpGet("/api/products")]
         public ActionResult GetProduct() {
+            _logger.LogInformation("Getting all products");
             var products = _productService.GetAllProducts();
             var productViewModels = products
                 .Select(ProductMapper.SerializeProductModel);
             return Ok(productViewModels);
+        }
+
+        /// <summary>
+        /// Archives an existing product
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpPatch("/api/products/{id}")]
+        public ActionResult ArchiveProduct(int id) {
+            _logger.LogInformation("Archiving product");
+            var archiveResult = _productService.ArchiveProduct(id);
+            return Ok(archiveResult);
         }
     }
 }

ef84c5f2dacff40a608c80f113f9100513c53790
Ubaid Rana <ubaidrana45@gmail.com>
2020-05-21 05:36:53 +0200

    Added SideMenu

---
+++ solarcoffee.frontend/src/components/SideMenu.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="side-menu-container"></div>
+</template>
+
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  name: "SideMenu",
+  components: {}
+})
+export default class SideMenu extends Vue {}
+</script>
+
+<style scoped lang="scss">
+@import "@/scss/global.scss";
+
+.side-menu-container {
+  background-color: #fcfcfc;
+  height: 100vh;
+  width: $menu-width;
+  display: flex;
+  flex-direction: column;
+  padding: 0.8rem;
+  box-shadow: 0 1px 1px rgba(0, 0, 0, 0.12), 0 1px 1px rgba(0, 0, 0, 0.24);
+  box-sizing: border-box;
+}
+</style>
